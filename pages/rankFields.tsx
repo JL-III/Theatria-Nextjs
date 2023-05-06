48d701c2677e324cefdfa0e3b78ed3c125a27568
JL-III <jesselopez1162@gmail.com>
2023-05-05 21:32:01 -0500

    type for rankfield

--- pages/rankFields.tsx
+++ pages/rankFields.tsx
@@ -1,5 +1,11 @@
 // rankFields.ts
-const rankFields = [
+interface RankField {
+    key: string;
+    label: string;
+    isButton: boolean;
+}
+
+const rankFields: RankField[] = [
     { key: "id", label: "ID", isButton: false },
     { key: "name", label: "Name", isButton: false },
     { key: "cost", label: "Cost", isButton: false },
